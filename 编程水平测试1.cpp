d44040e15efe2dcc7a2bc25216f46943b98cf223
xcgdCreeper <57204065+xcgdCreeper@users.noreply.github.com>
2019-12-15 15:37:52 +0800

    Add files via upload

---
+++ 编程水平测试1.cpp
@@ -0,0 +1,151 @@
+﻿
+#include <iostream>
+#include<cmath>
+#include<cstdlib>
+#include<cstring>
+#include<cstdio>
+#include<ctime>
+#define N 20                //因为20位大数的运算太过庞大，所以我测试的时候是用2位和3位的数，也就是N=2,N=3,经过测试，结果是正确的
+int a[N];
+int b[N] = { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 };
+int c[N];
+int d[N];
+void copy(int *x,int *y)  //x的值给y
+{
+	int i;
+	for (i = 0; i < N; i++)
+	{
+		y[i] = x[i];
+	}
+}
+void halfNum(int* x)    //x的一半      用于优化程序，除数只要累加到被除数的一半就行了，让程序的时间复杂度减半
+{
+	int i;
+	for (i = 0; i < N; i++)
+	{
+		//printf("step %d\n",i+1);
+		if (x[i] == 1 && i != (N - 1))
+		{
+			x[i] = 0;
+			x[i + 1] = x[i + 1] + 10;
+		}
+		else
+		{
+			if ((x[i] / 2) != ((x[i] + 1) / 2) && i != (N - 1))
+			{
+				x[i] = x[i] / 2;
+				//printf("hello\n");
+				x[i + 1] = x[i + 1] + 10;
+			}
+			else
+				x[i] = x[i] / 2;
+		}
+
+
+	}
+}
+void randNum() //生成随机数
+{
+	int i = 0;
+	srand((unsigned)time(NULL));
+	a[0] = rand() % 9 + 1;
+	for (i = 1; i < N; i++)
+	{
+		//srand((unsigned)time(NULL));
+		a[i] = rand() % 10;
+	}
+}
+
+void numPlus() //每次调用，除数+1
+{
+	int i;
+	b[N - 1]++;
+	for (i = N - 1; i >= 1; i--)
+	{
+		if ((b[i]) > 9)
+		{
+			b[i] = b[i] % 10;
+			//if (i == 0)
+			//break;
+			b[i - 1]++;
+			//printf("hello");
+		}
+	}
+}
+
+void numRed(int* x, int* y) //x-y
+{
+	int i;
+	for (i = N - 1; i >= 0; i--)
+	{
+		x[i] = x[i] - y[i];
+		if (i != 0)
+		{
+			if (x[i] < 0)
+			{
+				x[i] = 10 + x[i];
+				x[i - 1] = x[i - 1] - 1;
+			}
+		}
+	}
+}
+
+bool judge(int* x, int* y)// 判断x，y大小 返回0:x<=y   返回1:x>y    
+{
+	int i;
+	int mark = 0;
+	for (i = 0; i < N; i++)
+	{
+		if (x[i] > y[i])
+			return 1;
+		if (x[i] < y[i])
+			return 0;
+
+	}
+	return 0;
+}
+
+void printNum(int* a) //输出数字
+{
+	int i;
+	for (i = 0; i < N; i++)
+		printf("%d", a[i]);
+	printf("\n");
+}
+
+
+int main()
+{
+	int i;
+	int mark = 0;
+	randNum();
+	copy(a,c);
+	copy(c, d);
+	printf("a=");
+	printNum(a);
+	halfNum(d);
+	while (judge(d, b))
+	{
+		while (judge(c, b))
+		{
+			numRed(c, b);
+		}
+		for (i = 0; i < N; i++)
+		{
+			if (c[i] == b[i])
+				mark++;
+		}
+		if (mark == N)
+			break;
+		else
+			mark = 0;
+
+		copy(a,c);
+		numPlus();
+	}
+	if (mark == N)
+		printf("不是素数!");
+	else
+		printf("素数!");
+	return 0;
+}
---
+++ 编程水平测试2.cpp
@@ -0,0 +1,136 @@
+﻿// 编程水平测试2.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+#include <cstdio>
+#include<cstring>
+#include<cstdlib>
+
+using namespace std;
+
+struct people
+{
+	string name;
+	double score[3];
+	double sum;
+};
+
+struct team
+{
+	people student[5];
+	double avg[3];
+};
+
+team input()
+{
+	team a;
+	int i, j;
+	
+	for (i = 0; i < 5; i++)
+	{
+		//printf("%d:", i + 1);
+		cin >> a.student[i].name;
+		for (j = 0; j < 3; j++)
+		{
+			cin >> a.student[i].score[j];
+		}
+	}
+	cout << endl;
+	return a;
+}
+
+team sumScore(team a)
+{
+	int i, j;
+	double sum=0;
+	for (i = 0; i < 5; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			sum = sum + a.student[i].score[j];
+		}
+		a.student[i].sum = sum;
+		sum = 0;
+	}
+	return a;
+}
+
+team avgScore(team a)
+{
+	int i,j;
+	double avg=0;
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			avg = avg + a.student[j].score[i];
+		}
+		a.avg[i] = avg / 5;
+		avg = 0;
+	}
+	return a;
+}
+team order(team a)
+{
+	people tool;
+	int i, j;
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4 - i;j++)
+		{
+			if (a.student[j].sum < a.student[j + 1].sum)
+			{
+				tool = a.student[j];
+				a.student[j] = a.student[j + 1];
+				a.student[j + 1] = tool;
+			}
+		}
+	}
+	return a;
+}
+
+void output(team a)
+{
+	int i, j;
+	printf("\t 姓名\t语文\t数学\t外语\t总分\n");
+	cout << endl;
+	for (i = 0; i < 5; i++)
+	{
+		//printf("%s", a.student[i].name);
+		cout <<"\t "<< a.student[i].name << "\t";
+		for (j = 0; j < 3; j++)
+		{
+			//printf("%lf\t", a.student[i].score[j]);
+			cout << a.student[i].score[j]<<"\t";
+		}
+		cout << a.student[i].sum;
+		cout << endl;
+	}
+	printf("\t平均成绩 ");
+	for (i = 0; i < 3; i++)
+	{
+		cout<< a.avg[i];
+		if (i != 2)
+			cout << "\t";
+	}
+	cout << endl<<endl;
+}
+
+int main()
+{
+	printf("input姓名,语文成绩,数学成绩,外语成绩\n");
+	team group[3];
+	int i;
+	for (i = 0; i < 3; i++)
+	{
+		group[i] = input();
+		group[i] = sumScore(group[i]);
+		group[i] = avgScore(group[i]);
+		group[i] = order(group[i]);
+	}
+	for (i = 0; i < 3; i++)
+	{
+		printf("group%d:", i + 1);
+		output(group[i]);
+	}
+}
